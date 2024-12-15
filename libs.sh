5bc815727185bc70d5956a1bfd710ba8d6136fb1
Maksxpl <maks.kozon@gmail.com>
2024-12-15 06:24:43 +0100

    setup script + renamed waybar update_check module

---
+++ libs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+###
+### pacman
+###
+
+# Update system and package databases
+sudo pacman -Syu
+
+# Terminal utilities
+sudo pacman -S --needed alacritty ttf-hack-nerd zsh btop
+yay -S --needed neovim-git
+
+# Desktop
+sudo pacman -S --needed hyprland hyprpaper waybar swaync
+
+# Programs
+sudo pacman -S --needed thunar fuzzel
+
+# Audio tools
+sudo pacman -S --needed jack carla
+
+###
+### zsh plugins
+###
+
+# Oh-My-Zsh installation and plugin setup
+echo "Installing Oh-My-Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Zsh custom plugins directory
+ZSH_CUSTOM="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"
+
+# Install Zsh plugins
+echo "Installing Zsh plugins..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+git clone https://github.com/marlonrichert/zsh-autocomplete ${ZSH_CUSTOM}/plugins/zsh-autocomplete
+git clone https://github.com/jeffreytse/zsh-bat ${ZSH_CUSTOM}/plugins/zsh-bat
+
+# Reload Zsh configuration
+echo "Reloading Zsh..."
+source ~/.zshrc
+echo "Setup complete! Restart your terminal to ensure everything is loaded."
